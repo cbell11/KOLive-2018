dd03f084401e02f0ced3e6a797fdfdbf44f35eb5
cbell11 <chandler0522@gmail.com>
2019-01-22 12:22:59 +0700

    Update ko_game.js

--- ko_game.js
+++ ko_game.js
@@ -356,7 +356,7 @@ function shuffle(sourceArray) {
 
 
  var con = mysql.createConnection({
-   host: "162.241.252.113",
+   host: "knockout.fun",
    port: "3306",
    user: "knockoy5_cbell11",
    password: "Chandler0522!",
