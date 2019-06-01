2a95ba60bbd74193114838eca18ba5f30814046a
cbell11 <chandler0522@gmail.com>
2019-06-01 14:46:53 +0700

    Add files via upload

--- ko_game.js
+++ ko_game.js
@@ -503,22 +503,23 @@ function shuffle(sourceArray) {
 function populateQuestionPool(ko_id){
   var mysql = require('mysql');
   var express = require('express');
-  /*Local Host Setup
+  /*Local Host Setup*/
   var con = mysql.createConnection({
      host: "localhost",
      port: "3306",
      user: "root",
      password: "",
      database: "loginsystem",
-   });*/
+   });
      /*Online Setup*/
+     /*
      var con = mysql.createConnection({
       host: "127.0.0.1",
       port: "3306",
       user: "knockoy5_cbell11",
       password: "Chandler0522!",
       database: "knockoy5_WPZEL",
-    });
+    })*/
    con.connect(function(err) {
       if (err) throw err;
       console.log("Connected to mysql!");
